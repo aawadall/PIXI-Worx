ee03c3584ba66a9dca7ded75ae73641affed0c33
Ahmed Awadallah <aawadall@ualberta.ca>
2018-11-30 11:16:37 -0500

    loaded config via html

--- webpage/js/index.js
+++ webpage/js/index.js
@@ -6,7 +6,7 @@ window.onload = function () {
         resources = PIXI.resources,
         Sprite = PIXI.Sprite;
 
-    let config = require('./config.json');
+    
     console.debug("Version: " + config.version);
 
     let app = new Application({
