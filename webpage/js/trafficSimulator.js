67dd7adf5da6b57a4983f732620bf6d4e000863f
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-03 15:02:22 -0500

    commented PIXI.Stage, potentially to remove as deprecated

--- webpage/js/trafficSimulator.js
+++ webpage/js/trafficSimulator.js
@@ -1,8 +1,10 @@
 const Traffic = function (_width, _height) {
     this.renderer = new PIXI.CanvasRenderer(_width, _height);
     document.body.appendChild(this.renderer.view);
-    this.stage = new PIXI.Stage();
 
+    /*
+    this.stage = new PIXI.Stage(); // nominated for removal
+    */
     // setup Physics simulation
     this.world = new p2.World({
         gravity: [0, 0],
