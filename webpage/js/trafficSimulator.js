f17275cc2597c16f69d362d50e6e69b790eedd23
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-03 14:56:20 -0500

    empty traffic simulator to simulate smart traffic intersection

---
+++ webpage/js/trafficSimulator.js
@@ -0,0 +1,11 @@
+const Traffic = function (_width, _height) {
+    this.renderer = new PIXI.CanvasRenderer(_width, _height);
+    document.body.appendChild(this.renderer.view);
+    this.stage = new PIXI.Stage();
+
+    // setup Physics simulation
+    this.world = new p2.World({
+        gravity: [0, 0],
+
+    });
+}
