b1dd82263221f9d188895867a683a8b4119a53cf
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-10 14:39:59 -0500

    finally something is shown, fixed typo

--- webpage/js/trafficSimulator.js
+++ webpage/js/trafficSimulator.js
@@ -1,4 +1,5 @@
 const Traffic = function (_width, _height) {
+    console.log("DEBUG (Traffic) : Initialize");
     this.renderer = new PIXI.CanvasRenderer(_width, _height);
     this.renderer.backgroundColor = 0x555555;
     document.body.appendChild(this.renderer.view);
@@ -14,6 +15,7 @@ const Traffic = function (_width, _height) {
         gravity: [0, 0],
     });
 
+    console.log("DEBUG (Traffic) : Pre-Build function");
     this.build();
 
 };
@@ -33,20 +35,27 @@ Traffic.prototype = {
         // Each lane is are made of lines connecting two anchor points
         const anchors = roadNetwork.anchorPoints;
         const lanes = roadNetwork.lanes;
-        const laneWidth = 30;
+        const laneWidth = 10;
         const laneColor = 0x000000;
         const laneAlpha = 1.0;
 
+        console.log("DEBUG (buildRoadNetwork)");
         lanes.forEach( lane => {
                 let laneStart = anchors[lane.anchors[0]];
                 for(let idx=1; idx < lane.anchors.length; idx++ ) {
                     let laneEnd = anchors[lane.anchors[idx]];
+
                     // Draw Lanes
                     let laneSprite = new PIXI.Graphics();
                     laneSprite.lineStyle(laneWidth, laneColor, laneAlpha);
                     laneSprite.moveTo(laneStart.location.x, laneStart.location.y);
-                    laneSprite.lineTo(laneEnd.x, laneEnd.y);
+                    laneSprite.lineTo(laneEnd.location.x, laneEnd.location.y);
                     laneSprite.fillColor = laneColor;
+
+
+                    console.log("Line From (X,Y) : (" + laneStart.location.x + "," + laneStart.location.y + ")");
+                    console.log("Line To (X,Y) : (" + laneEnd.location.x + "," + laneEnd.location.x + ")");
+
                     this.scene.addChild(laneSprite);
 
                     //
